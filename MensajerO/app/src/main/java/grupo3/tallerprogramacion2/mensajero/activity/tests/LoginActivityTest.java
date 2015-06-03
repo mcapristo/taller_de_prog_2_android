a71dc90828eceb9dd0111d87673b67515fcb2789
Matias Capristo <matias.capristo@gmail.com>
2015-06-03 01:50:46 -0300

    Added success test case to LoginActivity

--- MensajerO/app/src/main/java/grupo3/tallerprogramacion2/mensajero/activity/tests/LoginActivityTest.java
+++ MensajerO/app/src/main/java/grupo3/tallerprogramacion2/mensajero/activity/tests/LoginActivityTest.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 
 import grupo3.tallerprogramacion2.mensajero.R;
 import grupo3.tallerprogramacion2.mensajero.activity.LoginActivity;
+import grupo3.tallerprogramacion2.mensajero.service.RestService;
 
 public class LoginActivityTest extends ActivityUnitTestCase<LoginActivity>{
 
@@ -30,10 +31,30 @@ public class LoginActivityTest extends ActivityUnitTestCase<LoginActivity>{
     }
 
     @SmallTest
-    public void testResponseProcess() {
+    public void test_errorResponse_processLoginResponse_ShowError() {
         try {
             loginActivity.processLoginResponse(getErrorResponse());
             assertEquals(loginActivity.getString(R.string.error_incorrect_password), mPasswordView.getError());
+        } catch (JSONException e) {
+            System.out.println("testResponseProcess :: Error");
+        }
+    }
+
+    @SmallTest
+    public void test_successResponse_processLoginResponse_navigateNextActivity() {
+        try {
+            loginActivity.processLoginResponse(getSuccessResponse());
+            final Intent launchIntent = getStartedActivityIntent();
+
+            assertNotNull("Intent was null", launchIntent);
+            assertTrue(isFinishCalled());
+
+            final String responseName = launchIntent.getStringExtra(RestService.LOGIN_RESPONSE_NAME);
+            final String responseToken = launchIntent.getStringExtra(RestService.LOGIN_TOKEN);
+
+            assertEquals("Username is wrong", "testuser", responseName);
+            assertEquals("Token is wrong", "123456", responseToken);
+
         } catch (JSONException e) {
             System.out.println("testResponseProcess :: Error");
         }
@@ -43,6 +64,20 @@ public class LoginActivityTest extends ActivityUnitTestCase<LoginActivity>{
         JSONObject response = new JSONObject();
         try {
             response.put("result", "ERROR");
+        } catch(JSONException e) {
+            System.out.println("Error building error response");
+        }
+        return response;
+    }
+
+    private JSONObject getSuccessResponse() {
+        JSONObject response = new JSONObject();
+        try {
+            response.put("result", "OK");
+            JSONObject data = new JSONObject();
+            data.put("username", "testuser");
+            data.put("token", "123456");
+            response.put("data", data);
         } catch(JSONException e) {
             System.out.println("Error building error response");
         }
